59ab3e7742d138ccb4daec44a90f2ed28d663926
iloktionov <iloktionov@skbkontur.ru>
2018-07-29 22:54:43 +0500

    Implemented TextToken.

--- Vostok.Logging.Formatting/Tokens/TextToken.cs
+++ Vostok.Logging.Formatting/Tokens/TextToken.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.IO;
+using JetBrains.Annotations;
 using Vostok.Logging.Abstractions;
 
 namespace Vostok.Logging.Formatting.Tokens
 {
     internal class TextToken : ITemplateToken
     {
+        private readonly string text;
+        private readonly int offset;
+        private readonly int length;
+
+        public TextToken([NotNull] string text, int offset, int length)
+        {
+            this.text = text ?? throw new ArgumentNullException(nameof(text));
+            this.offset = offset;
+            this.length = length;
+        }
+
         public void Render(LogEvent @event, TextWriter writer, IFormatProvider formatProvider)
         {
-            throw new NotImplementedException();
+            if (length == 0)
+                return;
+
+            if (length == text.Length)
+            {
+                writer.Write(text);
+            }
+            else
+            {
+                for (var index = 0; index < length; index++)
+                {
+                    writer.Write(text[offset + index]);
+                }
+            }
         }
     }
 }
