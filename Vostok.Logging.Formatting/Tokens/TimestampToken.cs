e2dc7cccf85496eaddd091376474fdf71c6d307d
iloktionov <iloktionov@skbkontur.ru>
2018-08-02 12:31:26 +0500

    First optimization pass for TimestampToken.

--- Vostok.Logging.Formatting/Tokens/TimestampToken.cs
+++ Vostok.Logging.Formatting/Tokens/TimestampToken.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using Vostok.Logging.Abstractions;
 
@@ -8,16 +10,76 @@ namespace Vostok.Logging.Formatting.Tokens
     [UsedImplicitly]
     internal class TimestampToken : NamedToken
     {
+        private static readonly IFormatProvider InvariantCulture = CultureInfo.InvariantCulture;
+
         public const string DefaultFormat = "yyyy-MM-dd HH:mm:ss,fff";
 
         public TimestampToken([CanBeNull] string format = null)
-            : base(PropertyNames.Timestamp, format ?? DefaultFormat)
+            : base(PropertyNames.Timestamp, format)
         {
         }
 
         public override void Render(LogEvent @event, TextWriter writer, IFormatProvider formatProvider)
         {
-            writer.Write(@event.Timestamp.ToString(Format, formatProvider));
+            var hasCustomFormat = Format != null && Format != DefaultFormat;
+            var hasCustomFormatProvider = formatProvider != null && !ReferenceEquals(formatProvider, InvariantCulture);
+
+            if (hasCustomFormat || hasCustomFormatProvider)
+            {
+                writer.Write(@event.Timestamp.ToString(Format, formatProvider));
+            }
+            else
+            {
+                FormatTimestampEfficiently(@event.Timestamp.DateTime, writer);
+            }
+        }
+
+        private static void FormatTimestampEfficiently(DateTime timestamp, TextWriter writer)
+        {
+            writer.Write(timestamp.Year.ToString(InvariantCulture));
+            writer.Write('-');
+
+            WriteNumberWithTwoDigitPadding(timestamp.Month, writer);
+            writer.Write('-');
+
+            WriteNumberWithTwoDigitPadding(timestamp.Day, writer);
+            writer.Write(' ');
+
+            WriteNumberWithTwoDigitPadding(timestamp.Hour, writer);
+            writer.Write(':');
+            
+            WriteNumberWithTwoDigitPadding(timestamp.Minute, writer);
+            writer.Write(':');
+            
+            WriteNumberWithTwoDigitPadding(timestamp.Second, writer);
+            writer.Write(',');
+            WriteNumberWithThreeDigitPadding(timestamp.Millisecond, writer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void WriteNumberWithTwoDigitPadding(int number, TextWriter writer)
+        {
+            if (number < 10)
+            {
+                writer.Write('0');
+            }
+
+            writer.Write(number.ToString(InvariantCulture));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void WriteNumberWithThreeDigitPadding(int number, TextWriter writer)
+        {
+            if (number < 10)
+            {
+                writer.Write("00");
+            }
+            else if (number < 100)
+            {
+                writer.Write('0');
+            }
+
+            writer.Write(number.ToString(InvariantCulture));
         }
     }
 }
