3e66ecb8a4af09a9e6d5e5af3be699e2aca55c6f
iloktionov <iloktionov@skbkontur.ru>
2018-09-26 19:55:59 +0500

    Switched to commons.formatting.

--- Vostok.Logging.Formatting/PropertyValueFormatter.cs
+++ Vostok.Logging.Formatting/PropertyValueFormatter.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using JetBrains.Annotations;
+using Vostok.Commons.Formatting;
 using Vostok.Logging.Formatting.Helpers;
 
 namespace Vostok.Logging.Formatting
@@ -16,7 +14,6 @@ namespace Vostok.Logging.Formatting
     public static class PropertyValueFormatter
     {
         private const int StringBuilderCapacity = 64;
-        private const int MaximumRecursionDepth = 3;
 
         /// <inheritdoc cref="Format(TextWriter,object,string,IFormatProvider)"/>
         public static string Format(
@@ -35,18 +32,7 @@ namespace Vostok.Logging.Formatting
         }
 
         /// <summary>
-        /// <para>Formats given property <paramref name="value"/>.</para>
-        /// <para>Here's how it works:</para>
-        /// <list type="number">
-        ///     <item><description>If <paramref name="value"/> is <c>null</c>, nothing happens. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/> is a <see cref="string"/>, it's written as is. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/> implements <see cref="IFormattable"/>, it's formatted using given <paramref name="format"/> and <paramref name="formatProvider"/>. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/>'s type explicitly overrides <see cref="Object.ToString"/>, we just use that. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/> implements <see cref="IReadOnlyDictionary{TKey,TValue}"/>, it's formatted as a JSON object. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/> implements <see cref="IEnumerable"/>, it's formatted as a JSON array. <para/></description></item>
-        ///     <item><description>If <paramref name="value"/>'s type has any properties with public getters, it's formatted as JSON object. <para/></description></item>
-        ///     <item><description>If nothing of above applies, default <see cref="Object.ToString"/> result is used. <para/></description></item>
-        /// </list>
+        /// <inheritdoc cref="ObjectValueFormatter.Format(TextWriter,object,string,IFormatProvider)"/>
         /// </summary>
         public static void Format(
             [NotNull] TextWriter writer,
@@ -66,112 +52,12 @@ namespace Vostok.Logging.Formatting
             if (insertLeadingSpace)
                 writer.WriteSpace();
 
-            FormatInternal(writer, value, format, formatProvider);
+            ObjectValueFormatter.Format(writer, value, format, formatProvider);
 
             if (insertTrailingSpace)
                 writer.WriteSpace();
         }
 
-        private static void FormatInternal(TextWriter writer, object value, string format, IFormatProvider formatProvider)
-        {
-            Type valueType;
-
-            if (value is string str)
-                writer.Write(str);
-            else if (value is IFormattable formattable)
-                writer.Write(formattable.ToString(format, formatProvider ?? CultureInfo.InvariantCulture));
-            else if (HasCustomToString(valueType = value.GetType()))
-                writer.Write(value.ToString());
-            else if (IsSimpleDictionary(valueType))
-                FormatDictionaryAsJson(writer, value, 1);
-            else if (value is IEnumerable enumerable)
-                FormatSequenceAsJson(writer, enumerable, 1);
-            else if (HasPublicProperties(valueType))
-                FormatObjectPropertiesAsJson(writer, value, 1);
-            else
-                writer.Write(value.ToString());
-        }
-
         private static void WriteSpace(this TextWriter writer) => writer.Write(" ");
-
-        private static bool HasCustomToString(Type type) =>
-            ToStringDetector.HasCustomToString(type);
-
-        private static bool IsSimpleDictionary(Type type) =>
-            DictionaryInspector.IsSimpleDictionary(type);
-
-        private static bool HasPublicProperties(Type type) =>
-            ObjectPropertiesExtractor.HasProperties(type);
-
-        private static void FormatAsJson(TextWriter writer, object value, int depth)
-        {
-            Type valueType;
-
-            if (value == null)
-                writer.Write("null");
-            else if (depth > MaximumRecursionDepth)
-                writer.Write("\"<too deep>\"");
-            else if (HasCustomToString(valueType = value.GetType()))
-                FormatValueAsJson(writer, value);
-            else if (IsSimpleDictionary(valueType))
-                FormatDictionaryAsJson(writer, value, depth);
-            else if (value is IEnumerable enumerable)
-                FormatSequenceAsJson(writer, enumerable, depth);
-            else if (HasPublicProperties(valueType))
-                FormatObjectPropertiesAsJson(writer, value, depth);
-            else FormatValueAsJson(writer, value);
-        }
-
-        private static void FormatDictionaryAsJson(TextWriter writer, object dictionary, int depth) =>
-            FormatPropertiesAsJson(writer, DictionaryInspector.EnumerateSimpleDictionary(dictionary), depth);
-
-        private static void FormatObjectPropertiesAsJson(TextWriter writer, object value, int depth) =>
-            FormatPropertiesAsJson(writer, ObjectPropertiesExtractor.ExtractProperties(value), depth);
-
-        private static void FormatSequenceAsJson(TextWriter writer, IEnumerable sequence, int depth)
-        {
-            writer.Write('[');
-
-            var delimiter = string.Empty;
-
-            foreach (var value in sequence)
-            {
-                writer.Write(delimiter);
-
-                FormatAsJson(writer, value, depth + 1);
-
-                delimiter = ", ";
-            }
-
-            writer.Write(']');
-        }
-
-        private static void FormatPropertiesAsJson(TextWriter writer, IEnumerable<(string, object)> properties, int depth)
-        {
-            writer.Write('{');
-
-            var delimiter = string.Empty;
-
-            foreach (var (name, value) in properties)
-            {
-                writer.Write(delimiter);
-                writer.Write('\"');
-                writer.Write(name);
-                writer.Write("\": ");
-
-                FormatAsJson(writer, value, depth + 1);
-
-                delimiter = ", ";
-            }
-
-            writer.Write('}');
-        }
-
-        private static void FormatValueAsJson(TextWriter writer, object value)
-        {
-            writer.Write('\"');
-            writer.Write(value.ToString());
-            writer.Write('\"');
-        }
     }
 }
