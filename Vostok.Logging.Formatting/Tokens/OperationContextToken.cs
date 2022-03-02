c93caa28554caec0b4e1ed02fa7381eb475b779c
Ilya Tzoop <ilyatzoop@gmail.com>
2022-03-02 11:31:48 +0500

    Fix namespaces

--- Vostok.Logging.Formatting/Tokens/OperationContextToken.cs
+++ Vostok.Logging.Formatting/Tokens/OperationContextToken.cs
@@ -3,23 +3,24 @@ using System.IO;
 using JetBrains.Annotations;
 using Vostok.Logging.Abstractions;
 
-namespace Vostok.Logging.Formatting.Tokens;
-
-internal class OperationContextToken : NamedToken
+namespace Vostok.Logging.Formatting.Tokens
 {
-    public OperationContextToken([CanBeNull] string format = null)
-        : base(WellKnownProperties.OperationContext, format)
+    internal class OperationContextToken : NamedToken
     {
-    }
+        public OperationContextToken([CanBeNull] string format = null)
+            : base(WellKnownProperties.OperationContext, format)
+        {
+        }
 
-    public override void Render(LogEvent @event, TextWriter writer, IFormatProvider formatProvider)
-    {
-        if (@event.Properties == null)
-            return;
+        public override void Render(LogEvent @event, TextWriter writer, IFormatProvider formatProvider)
+        {
+            if (@event.Properties == null)
+                return;
 
-        if (!@event.Properties.TryGetValue(Name, out var value))
-            return;
+            if (!@event.Properties.TryGetValue(Name, out var value))
+                return;
 
-        OperationContextValueFormatter.Format(@event, writer, value, Format, formatProvider);
+            OperationContextValueFormatter.Format(@event, writer, value, Format, formatProvider);
+        }
     }
 }
