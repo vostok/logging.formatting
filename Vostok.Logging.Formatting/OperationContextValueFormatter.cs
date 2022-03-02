3b5fa5adafae4a194342332706c6a288f0263f24
Ilya Tzoop <ilyatzoop@gmail.com>
2022-03-02 11:31:48 +0500

    Fix namespaces

--- Vostok.Logging.Formatting/OperationContextValueFormatter.cs
+++ Vostok.Logging.Formatting/OperationContextValueFormatter.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 using JetBrains.Annotations;
 using Vostok.Commons.Formatting;
 using Vostok.Logging.Abstractions;
@@ -8,64 +7,65 @@ using Vostok.Logging.Abstractions.Values;
 using Vostok.Logging.Formatting.Helpers;
 using Vostok.Logging.Formatting.Tokenizer;
 
-namespace Vostok.Logging.Formatting;
-
-[PublicAPI]
-public static class OperationContextValueFormatter
+namespace Vostok.Logging.Formatting
 {
-    private const int StringBuilderCapacity = 64;
+    [PublicAPI]
+    public static class OperationContextValueFormatter
+    {
+        private const int StringBuilderCapacity = 64;
     
-    public static string Format(
-        [NotNull] LogEvent @event,
-        [CanBeNull] object value,
-        [CanBeNull] string format = null,
-        [CanBeNull] IFormatProvider formatProvider = null)
-    {
-        if (@event == null)
-            throw new ArgumentNullException(nameof(@event));
-
-        var builder = StringBuilderCache.Acquire(StringBuilderCapacity);
-        var writer = new StringWriter(builder);
-
-        Format(@event, writer, value, format, formatProvider);
-
-        StringBuilderCache.Release(builder);
-
-        return builder.ToString();
-    }
-
-    public static void Format(
-        [NotNull] LogEvent @event,
-        [NotNull] TextWriter writer,
-        [CanBeNull] object value,
-        [CanBeNull] string format = null,
-        [CanBeNull] IFormatProvider formatProvider = null)
-    {
-        if (@event == null)
-            throw new ArgumentNullException(nameof(@event));
-        if (writer == null)
-            throw new ArgumentNullException(nameof(writer));
-
-        if (value is OperationContextValue contextValue)
-            value = contextValue.ToString();
-
-        if (value is not string template || !TemplateTokenizer.CanContainNamedTokens(template))
+        public static string Format(
+            [NotNull] LogEvent @event,
+            [CanBeNull] object value,
+            [CanBeNull] string format = null,
+            [CanBeNull] IFormatProvider formatProvider = null)
         {
-            PropertyValueFormatter.Format(writer, value, format, formatProvider);
-            return;
+            if (@event == null)
+                throw new ArgumentNullException(nameof(@event));
+
+            var builder = StringBuilderCache.Acquire(StringBuilderCapacity);
+            var writer = new StringWriter(builder);
+
+            Format(@event, writer, value, format, formatProvider);
+
+            StringBuilderCache.Release(builder);
+
+            return builder.ToString();
         }
 
-        var tokens = TemplatesCache.ObtainTokens(template);
+        public static void Format(
+            [NotNull] LogEvent @event,
+            [NotNull] TextWriter writer,
+            [CanBeNull] object value,
+            [CanBeNull] string format = null,
+            [CanBeNull] IFormatProvider formatProvider = null)
+        {
+            if (@event == null)
+                throw new ArgumentNullException(nameof(@event));
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
 
-        PaddingFormatHelper.TryParseFormat(format, out var insertLeadingSpace, out var insertTrailingSpace);
+            if (value is OperationContextValue contextValue)
+                value = contextValue.ToString();
 
-        if (insertLeadingSpace)
-            writer.WriteSpace();
+            if (value is not string template || !TemplateTokenizer.CanContainNamedTokens(template))
+            {
+                PropertyValueFormatter.Format(writer, value, format, formatProvider);
+                return;
+            }
 
-        foreach (var token in tokens)
-            token.Render(@event, writer, formatProvider);
+            var tokens = TemplatesCache.ObtainTokens(template);
 
-        if (insertTrailingSpace)
-            writer.WriteSpace();
+            PaddingFormatHelper.TryParseFormat(format, out var insertLeadingSpace, out var insertTrailingSpace);
+
+            if (insertLeadingSpace)
+                writer.WriteSpace();
+
+            foreach (var token in tokens)
+                token.Render(@event, writer, formatProvider);
+
+            if (insertTrailingSpace)
+                writer.WriteSpace();
+        }
     }
 }
--- Vostok.Logging.Formatting/Tokenizer/TemplatesCache.cs
+++ Vostok.Logging.Formatting/Tokenizer/TemplatesCache.cs
@@ -3,17 +3,18 @@ using System.Linq;
 using Vostok.Commons.Collections;
 using Vostok.Logging.Formatting.Tokens;
 
-namespace Vostok.Logging.Formatting.Tokenizer;
-
-internal static class TemplatesCache
+namespace Vostok.Logging.Formatting.Tokenizer
 {
-    private const int Capacity = 1000;
+    internal static class TemplatesCache
+    {
+        private const int Capacity = 1000;
 
-    private static readonly INamedTokenFactory TokenFactory = new PropertyTokensFactory();
+        private static readonly INamedTokenFactory TokenFactory = new PropertyTokensFactory();
 
-    private static readonly RecyclingBoundedCache<string, ITemplateToken[]> TemplateCache =
-        new RecyclingBoundedCache<string, ITemplateToken[]>(Capacity, StringComparer.Ordinal);
+        private static readonly RecyclingBoundedCache<string, ITemplateToken[]> TemplateCache =
+            new RecyclingBoundedCache<string, ITemplateToken[]>(Capacity, StringComparer.Ordinal);
 
-    public static ITemplateToken[] ObtainTokens(string template) =>
-        TemplateCache.Obtain(template, t => TemplateTokenizer.Tokenize(t, TokenFactory).ToArray());
+        public static ITemplateToken[] ObtainTokens(string template) =>
+            TemplateCache.Obtain(template, t => TemplateTokenizer.Tokenize(t, TokenFactory).ToArray());
+    }
 }
